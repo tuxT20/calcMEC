f500fb4a10683e79aca722e51b0989d790744f78
tuxT20 <147475263+tuxT20@users.noreply.github.com>
2024-01-18 18:03:14 -0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,26 +1,27 @@
+// Seu script JavaScript existente
 
+// Ajuste para calcular o desconto corretamente
 function calcTotal() {
   var itemTotal = 0;
- var discountPercentage = parseFloat(document.getElementById("discount").value) || 0; // 
+  var discountPercentage = parseFloat(document.getElementById("discount").value) || 0;
   var items = document.getElementsByTagName("input");
 
-
-  for (var i = 0; i < 43; i++) {
-    if (items[i].checked) {
-      itemTotal += items[i].value * 1/1000;
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].type === "checkbox" && items[i].checked) {
+      itemTotal += parseFloat(items[i].value);
     }
-   
-      var discountAmount = (itemTotal * discountPercentage) / 100;
-      itemTotal -= discountAmount;
-    
-    document.getElementById("total").innerHTML =
-      "Total:  $" + itemTotal + "k";
   }
+
+  var discountAmount = (itemTotal * discountPercentage) / 100;
+  itemTotal -= discountAmount;
+
+  document.getElementById("total").innerHTML = "Total:  $" + itemTotal.toFixed(2) + "k";
 }
 
-var calcButton = document.getElementById("calcButton"); 
-if (calcButton.addEventListener) {
-  calcButton.addEventListener("click", calcTotal, false); 
-} else if (calcButton.attachEvent) {
-  calcButton.attachEvent("onclick", calcTotal);
-}
+document.addEventListener("DOMContentLoaded", function () {
+  var calcButton = document.getElementById("calcButton");
+
+  if (calcButton) {
+    calcButton.addEventListener("click", calcTotal, false);
+  }
+});
